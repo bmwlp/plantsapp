a89060b3b1b6d77d82e043ac10705ffa6ddb6af8
bmwlp <walaipan.baimon@gmail.com>
2024-05-09 03:46:03 +0700

    ำ

--- index.js
+++ index.js
@@ -145,16 +145,17 @@ app.get('/favourite/get', (req, res) => {
         )
     })
     
-    app.delete('/favourite/:id', (req, res) => {
-        connection.query(
-            'DELETE FROM `fav` WHERE id =?',
-            [req.body.id],
-             function (err, results, fields) {
-                res.send(results)
-            }
-        )
-    })
-
+    app.delete('/favourite/:id', async (req, res) => {
+        const id = req.params.id;
+      
+        try {
+          await connection.query('DELETE FROM `fav` WHERE id = ?', [id]);
+          res.json({ message: `รายการโปรดที่มี id ${id} ถูกลบสำเร็จ` });
+        } catch (err) {
+          console.error(err);
+          res.status(500).json({ message: 'เกิดข้อผิดพลาดระหว่างการลบรายการโปรด' });
+        }
+      });
 app.listen(process.env.PORT || 3000, () => {
     console.log(' 3000')
 })
